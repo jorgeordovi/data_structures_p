b18d16fe78c3aa0c8368ac28fa9aafd1e61d7a7c
jorgeordovi <jorgeordovi@gmail.com>
2025-01-18 08:38:23 -0500

    Create strings_p.py

---
+++ strings_p.py
@@ -0,0 +1,7 @@
+#String types , '', "", """"
+string_simple = "Hi, Babe"
+string_doble =  "Hi, Bae"
+string_triple = '''this ex can be 
+multi-line'''
+string_triple2 ="""this text can be 
+mulitline as well"""
